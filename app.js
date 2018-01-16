19b4dc1124f41f6e198961580ac88fd83daf1f97
Nirali Shah <hellonirali@gmail.com>
2018-01-16 09:55:41 -0600

    finished rendering template

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const express = require( 'express' );
 const app = express();
+const nunjucks = require('nunjucks');
 
 
-app.listen(3000, () => console.log("we're listening.."));
+app.listen(3000, () => console.log("we're listening..."));
+
+app.use('/', (req, res, next) => {
+  console.log(`Request Type: ${req.method}`);
+  console.log(req.path);
+  next();
+});
 
 app.get('/', (req, res, next) => res.send("Hello!"));
+
+
+let locals = {
+  title: 'An Example',
+  people: [
+    {name: 'Gandalf'},
+    {name: 'Frodo'},
+    {name: 'Hermione'}
+  ]
+};
+
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, (err, output) => {
+  if (err) throw err;
+  console.log(output);
+});
+
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+app.get('/views/index.html', (req, res) => res.render( 'index', {title: 'Hall of Fame', people: people}));
