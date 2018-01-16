e63c6dc2a2fa56faca5415c5ae0686985312db70
Nirali Shah <hellonirali@gmail.com>
2018-01-16 11:47:32 -0600

    finished live test

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+
 const express = require( 'express' );
 const app = express();
+const routes = require('./routes');
 const nunjucks = require('nunjucks');
 
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+nunjucks.configure('views', {noCache: true});
 
-app.listen(3000, () => console.log("we're listening..."));
+app.use(express.static('public'));
+
+app.use('/', routes);
 
 app.use('/', (req, res, next) => {
   console.log(`Request Type: ${req.method}`);
@@ -13,24 +20,23 @@ app.use('/', (req, res, next) => {
 
 app.get('/', (req, res, next) => res.send("Hello!"));
 
+app.listen(3000, () => console.log("we're listening..."));
 
-let locals = {
-  title: 'An Example',
-  people: [
-    {name: 'Gandalf'},
-    {name: 'Frodo'},
-    {name: 'Hermione'}
-  ]
-};
-
-app.engine('html', nunjucks.render);
-app.set('view engine', 'html');
-nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, (err, output) => {
-  if (err) throw err;
-  console.log(output);
-});
+// let locals = {
+//   title: 'An Example',
+//   people: [
+//     {name: 'Gandalf'},
+//     {name: 'Frodo'},
+//     {name: 'Hermione'}
+//   ]
+// };
 
 
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-app.get('/views/index.html', (req, res) => res.render( 'index', {title: 'Hall of Fame', people: people}));
+// nunjucks.render('index.html', locals, (err, output) => { //refers to the terminal
+//   if (err) throw err;
+//   console.log(output);
+// });
+
+
+// const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// app.get('/views/index.html', (req, res) => res.render( 'index', {title: 'Hall of Fame', people: people})); //referring to the browser
